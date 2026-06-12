b78965c65514ccd17131092ac74b8f2abd70a3a8
whyme <wavesauce.booking@gmail.com>
2026-06-13 00:44:00 +0300

    Bugfix | PlainTextResponse

--- routes/metrics.py
+++ routes/metrics.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
 from utils import get_metrics, process_metrics
 
 router = APIRouter()
@@ -8,5 +9,8 @@ async def metrics_wrapper():
     data = await get_metrics()
     data = await process_metrics(data)
     
-    return data
+    return PlainTextResponse(
+        content=data,
+        status_code=200,
+    )
 
